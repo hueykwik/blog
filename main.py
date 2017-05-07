3e0f4f60a4283314749135ee2ee85ac32e258864
Huey Kwik <huey.kwik@gmail.com>
2017-05-06 18:37:15 -0700

    Add skeleton handlers for /blog and /newpost.

--- main.py
+++ main.py
@@ -15,12 +15,19 @@
 import webapp2
 
 
-class MainPage(webapp2.RequestHandler):
+class BlogHandler(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        self.response.write('Hello, Blog!')
+
+
+class NewPostHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, NewPost!')
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/blog', BlogHandler),
+    ('/newpost', NewPostHandler)
 ], debug=True)
