e18d37335dcf50cd136b2770b0cadc2b1b8acbaa
Huey Kwik <huey.kwik@gmail.com>
2017-05-16 20:52:23 -0700

    Add comment for BlogPost class.

--- model.py
+++ model.py
@@ -2,6 +2,13 @@ from google.appengine.ext import db
 
 
 class BlogPost(db.Model):
+    """Models a blog post.
+
+    Attributes:
+        subject: The blog post title.
+        content: The blog post text.
+        created: When the blog post was created.
+    """
     subject = db.StringProperty(required=True)
     content = db.TextProperty(required=True)
     created = db.DateTimeProperty(auto_now_add=True)
@@ -17,9 +24,9 @@ class User(db.Model):
     """Models a user.
 
     Attributes:
-        username: The user's username
+        username: The user's username.
         password: The user's hashed and salted password.
-        email: The user's email
+        email: The user's email.
     """
     username = db.StringProperty(required=True)
     hash_password = db.StringProperty(required=True)
