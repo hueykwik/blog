cb981b41ba14bc2e8f7c0352caa892fd078254ff
Huey Kwik <huey.kwik@gmail.com>
2017-05-18 23:07:07 -0700

    Add comment model.

--- model.py
+++ model.py
@@ -31,4 +31,18 @@ class BlogPost(db.Model):
         return self.content.replace('\n', '<br>')
 
 
+class Comment(db.Model):
+    """Models a blog comment.
 
+    Attributes:
+        content: The comment text.
+        created: When the commment was created.
+        author: The user who wrote the comment.
+        post: The comment's associated blog post.
+
+    """
+
+    content = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+    author = db.ReferenceProperty(User, required=True, collection_name="comments")
+    post = db.ReferenceProperty(BlogPost, required=True, collection_name="comments")
